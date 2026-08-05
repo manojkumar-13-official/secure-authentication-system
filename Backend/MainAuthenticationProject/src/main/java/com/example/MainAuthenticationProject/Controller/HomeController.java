1369b9982ba58fb464fe45a37921b7b04bbda7ef
manojkumar-13-official <manojkumaratarumugam@gmail.com>
2026-08-05 11:46:21 +0530

    Home Controller

---
+++ Backend/MainAuthenticationProject/src/main/java/com/example/MainAuthenticationProject/Controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.example.MainAuthenticationProject.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping
+    public String getGreetings(){
+        return "Welcome to my project";
+    }
+}
